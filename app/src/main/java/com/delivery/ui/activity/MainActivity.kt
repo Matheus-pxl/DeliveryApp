ea9d97c62f21ecd3480174ebeeb7e6279a4b0377
Matheus <matheushellf@gmail.com>
2024-06-04 14:12:02 -0300

    design do ProductItem

--- app/src/main/java/com/delivery/ui/activity/MainActivity.kt
+++ app/src/main/java/com/delivery/ui/activity/MainActivity.kt
@@ -7,15 +7,27 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.delivery.ui.activity.ui.theme.DeliveryTheme
+import com.delivery.ui.activity.ui.theme.Purple40
+import com.delivery.ui.activity.ui.theme.Purple80
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,15 +45,60 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun ProductItem() {
-    Column {
-        Box(modifier = Modifier
-            .height(50.dp)
-            .width(50.dp)
-            .height(50.dp)
-            .background(Color.Blue))
-        Image(painter = painterResource(id = R.drawable.ic_launcher_background ), contentDescription = null)
-        Text("texto 1")
-        Text("texto 1")
+    Surface(
+        Modifier.padding(10.dp),
+        shape = RoundedCornerShape(12.dp),
+        shadowElevation = 4.dp
+    ) {
+        Column(
+            Modifier
+                .heightIn(250.dp, 300.dp)
+                .width(200.dp)
+        ) {
+            val imageSize = 100.dp
+            Box(
+                modifier = Modifier
+                    .height(imageSize)
+                    .fillMaxWidth()
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Purple40,
+                                Purple80
+                            )
+                        )
+                    )
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = null,
+                    Modifier
+                        .offset(y = imageSize/2)
+                        .size(imageSize)
+                        .clip(shape = CircleShape)
+                        .align(Alignment.BottomCenter)
+                )
+            }
+            Spacer(modifier = Modifier.height(imageSize/2))
+
+            Column(Modifier.padding(16.dp)) {
+                Text(
+                    text = LoremIpsum(50).values.first(),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight((700)),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    "R$ 15,99",
+                    Modifier.padding(top = 4.dp),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400)
+                )
+
+            }
+
+        }
 
     }
 }
